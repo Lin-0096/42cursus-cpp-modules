f3e12c73fa613a6c5d9a6905b991f57dd6f14e5f
Lin Liu <liulin0096@gmail.com>
2025-10-23 08:51:47 +0000

    ignore

---
+++ cpp01/ex03/WeaponA.hpp
@@ -0,0 +1,19 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <string>
+#include <iostream>
+
+class Weapon
+{
+private:
+	std::string type;
+
+public:
+	Weapon();
+	~Weapon();
+	const std::string getType() const;
+
+};
+
+#endif
