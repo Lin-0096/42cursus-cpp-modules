d3c5eee29beadbbf47a9427d7de7c8e638a8a24c
Lin Liu <liulin0096@gmail.com>
2025-10-24 08:21:51 +0000

    ex03

--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -10,10 +10,11 @@ private:
 	std::string type;
 
 public:
-	Weapon();
+	//Weapon(); //??
+	Weapon(std::string type);
 	~Weapon();
-	const std::string getType() const;
-
+	const	std::string& getType() const;
+	void	setType(std::string newType);
 };
 
 #endif
