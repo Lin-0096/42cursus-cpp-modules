4228832cd7ec31376dedafb7993d9ddecef50d39
Lin Liu <liulin0096@gmail.com>
2025-11-02 20:42:30 +0200

    exo2

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,23 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+	~Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat( void ) const;
+	int toInt( void ) const;
+
+private:
+	int _rawBits;
+	static const int _fractionalBits = 8;
+};
+
+#endif
